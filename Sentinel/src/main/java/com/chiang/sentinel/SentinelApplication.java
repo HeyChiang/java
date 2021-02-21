490383437056daac34d3471ff02796686f324795
Chiang <Chiang09021>
2021-02-21 22:27:34 +0800

    add sentinel project

---
+++ Sentinel/src/main/java/com/chiang/sentinel/SentinelApplication.java
@@ -0,0 +1,16 @@
+package com.chiang.sentinel;
+
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentinelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SentinelApplication.class, args);
+    }
+}
+
+
+
