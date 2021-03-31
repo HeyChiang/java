84a79da7e90fbee345ee6927429d4607a639d201
jianghao <125354243@qq.com>
2021-03-31 10:39:15 +0800

    add read and write interceptor

---
+++ mybatis/src/main/java/com/mybatis/intercept/MasterSlaveAutoRoutingPlugin.java
@@ -0,0 +1,51 @@
+package com.mybatis.intercept;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.executor.Executor;
+import org.apache.ibatis.mapping.*;
+import org.apache.ibatis.plugin.*;
+import org.apache.ibatis.session.ResultHandler;
+import org.apache.ibatis.session.RowBounds;
+
+import java.util.Properties;
+
+/**
+ * 读写分离自动拦截
+ * @author jianghao
+ */
+@Intercepts({@Signature(type = Executor.class, method = "query", args = {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class}),
+        @Signature(type = Executor.class, method = "update", args = {MappedStatement.class, Object.class})})
+@Slf4j
+public class MasterSlaveAutoRoutingPlugin implements Interceptor {
+
+    private static final String MASTER = "master";
+
+    private static final String SLAVE = "slave";
+
+    @Override
+    public Object intercept(Invocation invocation) throws Throwable {
+        Object[] args = invocation.getArgs();
+        MappedStatement ms = (MappedStatement) args[0];
+        SqlSource sqlSource =  ms.getSqlSource();
+        BoundSql boundSql = sqlSource.getBoundSql("sqlSource");
+        String sql = boundSql.getSql();
+        System.out.println(sql);
+
+//        try {
+//        切换读写库
+//            DynamicDataSourceContextHolder.push(SqlCommandType.SELECT == ms.getSqlCommandType() ? SLAVE : MASTER);
+            return invocation.proceed();
+//        } finally {
+//            DynamicDataSourceContextHolder.clear();
+//        }
+    }
+
+    @Override
+    public Object plugin(Object target) {
+        return target instanceof Executor ? Plugin.wrap(target, this) : target;
+    }
+
+    @Override
+    public void setProperties(Properties properties) {
+    }
+}
--- src/main/java/com/chiang/test/Main.java
+++ src/main/java/com/chiang/test/Main.java
@@ -6,8 +6,11 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i < 55; i++) {
-            System.out.println(i%10);
-        }
+        long s1 = System.currentTimeMillis();
+        String str = "select * FROM dj_store_xx left join haha where id=1";
+        boolean matches = str.matches(".+(?i)from\\s+(?i)dj_store_.+");
+        long s2 = System.currentTimeMillis();
+        System.out.println(s2-s1);
+        System.out.println(matches);
     }
 }
