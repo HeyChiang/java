fdd5c1a197c8605998da2c588ab60068bb78a4f4
jianghao <125354243@qq.com>
2021-09-10 10:32:31 +0800

    删除无用的代码，增加app name

--- gateway/src/main/java/com/gateway/demo/BodyRewrite.java
+++ gateway/src/main/java/com/gateway/demo/BodyRewrite.java
@@ -2,26 +2,23 @@ package com.gateway.demo;
 
 import org.reactivestreams.Publisher;
 import org.springframework.cloud.gateway.filter.factory.rewrite.RewriteFunction;
-import org.springframework.cloud.gateway.support.ServerWebExchangeUtils;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
-import java.nio.charset.StandardCharsets;
-
+/**
+ * 重写内容
+ *
+ * @author jianghao
+ */
 @Component
 public class BodyRewrite implements RewriteFunction<byte[], byte[]> {
     @Override
     public Publisher<byte[]> apply(ServerWebExchange exchange, byte[] body) {
+
         String originalBody = body == null ? "" : new String(body);
+        System.out.println("BodyRewrite body：" + originalBody);
 
-        System.out.println("BodyRewrite - 返回的内容："+originalBody);
-
-        if (!ServerWebExchangeUtils.isAlreadyRouted(exchange)) {
-            return Mono.just(originalBody.getBytes());
-        } else {
-            // its the reponse body when already routed
-            return Mono.just(originalBody.getBytes(StandardCharsets.UTF_8));
-        }
+        return Mono.just(originalBody.getBytes());
     }
 }
