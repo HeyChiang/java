e4443f6f5eb3ed1ed672d09c0ed171282df8759a
jianghao <125354243@qq.com>
2022-01-06 20:27:06 +0800

    add StampedLockDemo and lock.newCondition

--- src/main/java/com/chiang/java/thread/lock/ReentrantLockDemo.java
+++ src/main/java/com/chiang/java/thread/lock/ReentrantLockDemo.java
@@ -1,7 +1,10 @@
 package com.chiang.java.thread.lock;
 
-import java.util.concurrent.locks.ReadWriteLock;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
+import lombok.SneakyThrows;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.locks.*;
 
 /**
  * 1.重复的lock和unlock不会报错
@@ -10,16 +13,36 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
  * @author Chiang
  */
 public class ReentrantLockDemo {
-    // 初始化一个读写锁
-    private final ReadWriteLock rwlock = new ReentrantReadWriteLock();
 
     public static void main(String[] args) {
+//        readAndWrite();
+
+        lockConditionTest();
+    }
+
+    /**
+     * lock.newCondition的lock与synchronize的wait是一样的
+     * signal=notify
+     * signalAll=notifyAll
+     */
+    private static void lockConditionTest() {
+        LockCondition lockCondition = new LockCondition();
+        new Thread(lockCondition::getTask).start();
+        new Thread(()->{lockCondition.addTask("A");}).start();
+
+    }
+
+    /**
+     * 读写锁互斥，不能同步操作
+     */
+    private static void readAndWrite() {
         ReentrantLockDemo readWriteLockMain = new ReentrantLockDemo();
         new Thread(readWriteLockMain::writeMethod).start();
         new Thread(readWriteLockMain::readMethod).start();
         new Thread(readWriteLockMain::readMethod).start();
     }
 
+    private final ReadWriteLock rwlock = new ReentrantReadWriteLock();
     private void readMethod() {
         rwlock.readLock().lock();
         System.out.println(Thread.currentThread().getName() + " 获得了读锁");
@@ -49,6 +72,36 @@ public class ReentrantLockDemo {
         } finally {
             rwlock.writeLock().unlock();
             System.out.println(Thread.currentThread().getName() + " 释放了写锁");
+        }
+    }
+
+
+    static class LockCondition {
+        private final Lock lock = new ReentrantLock();
+        private final Condition condition = lock.newCondition();
+        private final Queue<String> queue = new LinkedList<>();
+
+        public void addTask(String s) {
+            lock.lock();
+            try {
+                queue.add(s);
+                condition.signalAll();
+            } finally {
+                lock.unlock();
+            }
+        }
+
+        @SneakyThrows
+        public String getTask() {
+            lock.lock();
+            try {
+                while (queue.isEmpty()) {
+                    condition.await();
+                }
+                return queue.remove();
+            } finally {
+                lock.unlock();
+            }
         }
     }
 }
--- src/main/java/com/chiang/java/thread/lock/StampedLockDemo.java
+++ src/main/java/com/chiang/java/thread/lock/StampedLockDemo.java
@@ -2,24 +2,36 @@ package com.chiang.java.thread.lock;
 
 import java.util.concurrent.locks.StampedLock;
 
+/**
+ * StampLock比起ReentrantLock多了乐观锁，在读写使用乐观锁去读取参数，在写的场景少的情况下，
+ * 使用StampLock更好，JDK 1.8版本开始启用
+ * @author jianghao
+ */
 public class StampedLockDemo {
     private final StampedLock stampedLock = new StampedLock();
 
     private double x;
     private double y;
 
+    /**
+     * 获取写锁，写入参数
+     */
     public void move(double deltaX, double deltaY) {
-        long stamp = stampedLock.writeLock(); // 获取写锁
+        long stamp = stampedLock.writeLock();
         try {
             x += deltaX;
             y += deltaY;
         } finally {
-            stampedLock.unlockWrite(stamp); // 释放写锁
+            stampedLock.unlockWrite(stamp);
         }
     }
 
+    /**
+     * 使用乐观锁去获取参数，然后对比乐观锁版本，版本有变化则开启悲观锁
+     */
     public double distanceFromOrigin() {
-        long stamp = stampedLock.tryOptimisticRead(); // 获得一个乐观读锁
+        // 获得一个乐观读锁
+        long stamp = stampedLock.tryOptimisticRead();
         // 注意下面两行代码不是原子操作
         // 假设x,y = (100,200)
         double currentX = x;
@@ -27,13 +39,16 @@ public class StampedLockDemo {
         double currentY = y;
         // 此处已读取到y，如果没有写入，读取是正确的(100,200)
         // 如果有写入，读取是错误的(100,400)
-        if (!stampedLock.validate(stamp)) { // 检查乐观读锁后是否有其他写锁发生
-            stamp = stampedLock.readLock(); // 获取一个悲观读锁
+        // 检查乐观读锁后是否有其他写锁发生
+        if (!stampedLock.validate(stamp)) {
+            // 获取一个悲观读锁
+            stamp = stampedLock.readLock();
             try {
                 currentX = x;
                 currentY = y;
             } finally {
-                stampedLock.unlockRead(stamp); // 释放悲观读锁
+                // 释放悲观读锁
+                stampedLock.unlockRead(stamp);
             }
         }
         return Math.sqrt(currentX * currentX + currentY * currentY);
