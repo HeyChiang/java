a91b7e677321acd317abaa690d5a2c1db8011e54
jianghao <125354243@qq.com>
2022-01-14 19:47:14 +0800

    add Volatile example

--- src/main/java/com/chiang/java/thread/Volatile.java
+++ src/main/java/com/chiang/java/thread/Volatile.java
@@ -1,30 +1,38 @@
 package com.chiang.java.thread;
 
+import lombok.SneakyThrows;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * volatile 变量直接操作内存，不再使用CPU的缓存,
+ * 可以保证读取到的变量值是最新的，但是不保证操作的原子性
+ * @author JiangHao
+ */
+@SuppressWarnings("all")
 public class Volatile {
-    static class Work {
-        boolean isShutDown = false;
+    private CountDownLatch latch = new CountDownLatch(100);
+    public volatile int inc = 0;
 
-        void shutdown() {
-            isShutDown = true;
-            System.out.println("shutdown! " + Thread.currentThread().getName());
-        }
-
-        void doWork() {
-            while (!isShutDown) {
-                System.out.println("doWork " + Thread.currentThread().getName());
-            }
-        }
+    public void increase() {
+        inc++;
     }
 
+    @SneakyThrows
     public static void main(String[] args) {
-        Work work = new Work();
+        final Volatile test = new Volatile();
+        for (int i = 0; i < 100; i++) {
+            new Thread(() -> {
+                for (int j = 0; j < 1000; j++) {
+                    test.increase();
+                }
+                test.latch.countDown();
+            }).start();
+        }
 
-        new Thread(work::doWork).start();
-        new Thread(work::doWork).start();
-        new Thread(work::doWork).start();
-        new Thread(work::shutdown).start();
-        new Thread(work::doWork).start();
-        new Thread(work::doWork).start();
-        new Thread(work::doWork).start();
+        //保证前面的线程都执行完
+        test.latch.await();
+        System.out.println(test.inc);
     }
 }
