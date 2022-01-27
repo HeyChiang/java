e10e7c70132f2ef14725da78cde0b3204b138f1d
jianghao <125354243@qq.com>
2022-01-27 17:18:22 +0800

    add InsertSort example

--- src/main/java/com/chiang/algorithm/foundation/insert/InsertSort.java
+++ src/main/java/com/chiang/algorithm/foundation/insert/InsertSort.java
@@ -12,38 +12,34 @@ import java.util.Arrays;
 public class InsertSort {
 
     public static void main(String[] args) {
-        Integer[] testTime = new Integer[]{10};
+        Integer[] testTime = new Integer[]{10000,100000};
 
         for (int k = 0; k < testTime.length; k++) {
             Integer[] array = SortHelper.generateArray(testTime[k]);
             long startTime = System.currentTimeMillis();
 
             // array[0..i)是已排序的，array[i..n)是未排序的
-//            sortNormal(array);
-            sortOptimize(array);
+            sortNormal(array);
+//            sortOptimize(array);
             double useTime = (System.currentTimeMillis() - startTime)/1000.0;
             System.out.println("run：" + testTime[k] + " time：" + useTime +"s");
         }
     }
 
     /**
-     * 优化，减少不断交换的数组数据的寻址操作
+     * 优化，减少不断交换的数组数据的寻址操作，赋值只有一次
      */
     private static void sortOptimize(Integer[] array) {
-        System.out.println("排序前：");
-        Arrays.stream(array).forEach(s -> System.out.print(s+" "));
         for (int i = 0; i < array.length; i++) {
             int temp = array[i];
             int j;
+
+            // 特点： >temp时候会提前终止循环，而普通排序则会整个循环遍历完成
             for (j = i; j-1 >= 0 && array[j-1] > temp; j--) {
                 array[j] = array[j-1];
             }
             array[j] = temp;
         }
-        System.out.println();
-        System.out.println("排序后：");
-        Arrays.stream(array).forEach(s -> System.out.print(s+" "));
-        System.out.println();
     }
 
     /**
@@ -52,6 +48,7 @@ public class InsertSort {
     private static void sortNormal(Integer[] array) {
         for (int i = 0; i < array.length; i++) {
             for (int j = i; j-1 >= 0 ; j--) {
+                // 一定会把j遍历完成
                 if(array[i].compareTo(array[j]) > 0){
                     swap(i,j,array);
                 }
@@ -60,7 +57,7 @@ public class InsertSort {
     }
 
     /**
-     * 交换数组内参数的位置
+     * 交换数组内参数的位置，每次交换有三次操作
      */
     private static void swap(int i, int j, Integer[] array) {
         int temp  = array[i];
