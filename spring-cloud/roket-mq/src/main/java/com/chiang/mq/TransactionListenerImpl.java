d0abccf6e1e85306aa12c4450b81d927f0865dde
jianghao <125354243@qq.com>
2022-02-17 17:44:31 +0800

    add mq example

--- spring-cloud/roket-mq/src/main/java/com/chiang/mq/TransactionListenerImpl.java
+++ spring-cloud/roket-mq/src/main/java/com/chiang/mq/TransactionListenerImpl.java
@@ -8,29 +8,39 @@ import org.apache.rocketmq.common.message.MessageExt;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
+/**
+ * 以事务的形式发送消息
+ *
+ * @author Chiang
+ */
 public class TransactionListenerImpl implements TransactionListener {
-  private AtomicInteger transactionIndex = new AtomicInteger(0);
-  private ConcurrentHashMap<String, Integer> localTrans = new ConcurrentHashMap<>();
-  @Override
-  public LocalTransactionState executeLocalTransaction(Message msg, Object arg) {
-      int value = transactionIndex.getAndIncrement();
-      int status = value % 3;
-      localTrans.put(msg.getTransactionId(), status);
-      return LocalTransactionState.UNKNOW;
-  }
-  @Override
-  public LocalTransactionState checkLocalTransaction(MessageExt msg) {
-      Integer status = localTrans.get(msg.getTransactionId());
-      if (null != status) {
-          switch (status) {
-              case 0:
-                  return LocalTransactionState.UNKNOW;
-              case 1:
-                  return LocalTransactionState.COMMIT_MESSAGE;
-              case 2:
-                  return LocalTransactionState.ROLLBACK_MESSAGE;
-          }
-      }
-      return LocalTransactionState.COMMIT_MESSAGE;
-  }
+
+    private final AtomicInteger transactionIndex = new AtomicInteger(0);
+    private final ConcurrentHashMap<String, Integer> localTrans = new ConcurrentHashMap<>();
+
+    @Override
+    public LocalTransactionState executeLocalTransaction(Message msg, Object arg) {
+        int value = transactionIndex.getAndIncrement();
+        int status = value % 3;
+        localTrans.put(msg.getTransactionId(), status);
+        return LocalTransactionState.UNKNOW;
+    }
+
+    @Override
+    public LocalTransactionState checkLocalTransaction(MessageExt msg) {
+        Integer status = localTrans.get(msg.getTransactionId());
+
+        if (null != status) {
+            switch (status) {
+                case 0:
+                    return LocalTransactionState.UNKNOW;
+                case 1:
+                    return LocalTransactionState.COMMIT_MESSAGE;
+                case 2:
+                    return LocalTransactionState.ROLLBACK_MESSAGE;
+                default:
+            }
+        }
+        return LocalTransactionState.COMMIT_MESSAGE;
+    }
 }
