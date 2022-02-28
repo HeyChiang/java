7d71cecca329990d69202f8d1579a279a02b6e2a
jianghao <125354243@qq.com>
2022-02-28 12:39:51 +0800

    add iteration

--- src/main/java/com/chiang/algorithm/linked/ReverseList.java
+++ src/main/java/com/chiang/algorithm/linked/ReverseList.java
@@ -5,22 +5,39 @@ package com.chiang.algorithm.linked;
  */
 public class ReverseList {
 
-    public class ListNode {
+    public static class ListNode {
         int val;
         ListNode next;
         ListNode() {}
         ListNode(int val) { this.val = val; }
         ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+        @Override
+        public String toString() {
+            StringBuilder builder = new StringBuilder();
+
+            ListNode curNode = this;
+            while (curNode != null){
+                builder.append(curNode.val).append(" -> ");
+                curNode = curNode.next;
+            }
+            builder.append("NULL");
+
+            return "LinkedList{" +
+                    "data=" + builder +
+                    '}';
+        }
     }
 
     /**
-     *  5、4、3、2、1、null -> null、1、2、3、4、5
+     * 链表反转： 5、4、3、2、1、null -> null、1、2、3、4、5
      * 同时保存cur、pre、next三个元素，不断前移替换。
-     *  1. 把 next = pre ，指向下一个的变成指向前面一个
-     *  2. 把 pre = cur
-     *  3. cur =
+     *  1. 保存cur.next 临时变量，避免下一个指向前一个而导致下一个找不到节点了
+     *  2. 把 cur.next = pre ，指向下一个的变成指向前面一个，没有pre也就无法指向上一个了
+     *  3. 把 pre = cur ,上一个变成当前，重复的将下一个指向前一个，不断前移替换
+     *  4. cur = next ,当前的变量指向下一个，不断前移替换
      */
-    public ListNode reverseList(ListNode head) {
+    public ListNode reverseList1(ListNode head) {
         ListNode cur = head;
         ListNode pre = null;
 
@@ -31,5 +48,32 @@ public class ReverseList {
             cur = next;
         }
         return pre;
+    }
+
+    public ListNode reverseList2(ListNode head){
+        if(head == null || head.next == null){
+            return head;
+        }
+
+        ListNode rev = reverseList2(head);
+        rev.next.next = head;
+        head.next = null;
+
+        return rev;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode();
+        ListNode cur = head;
+        for (int i = 1; i < 6; i++) {
+            cur.next = new ListNode(i);
+            cur = cur.next;
+        }
+
+        System.out.println("正序：");
+        System.out.println(head);
+        ListNode node = new ReverseList().reverseList1(head);
+        System.out.println("倒叙：");
+        System.out.println(node);
     }
 }
