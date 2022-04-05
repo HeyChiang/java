6755293fca32ce60a77ccff1ab678c7d9fc06d69
蒋豪 <125354243@qq.com>
2022-04-05 22:47:32 +0800

    完善递归排序

--- src/main/java/com/chiang/algorithm/merge/MergeSort.java
+++ src/main/java/com/chiang/algorithm/merge/MergeSort.java
@@ -4,31 +4,47 @@ import java.util.Arrays;
 
 public class MergeSort {
     public static void main(String[] args) {
+        Integer[] arrays = new Integer[]{98, 3, 4, 6, 5, 1, 2};
+        MergeSort.sort(arrays, 0, arrays.length - 1);
+        for (Integer array : arrays) {
+            System.out.println(array);
+        }
 
     }
 
-    private <E extends Comparable<E>> void sort(E[] array, int l, int r) {
-        if (r >= l) {
+    private static <E extends Comparable<E>> void sort(E[] array, int l, int r) {
+        if (l >= r) {
             return;
         }
-        // l可能是1w，r是2w
+
         int mid = l + (r - l) / 2;
         sort(array, l, mid);
-        sort(array, mid, r);
+        sort(array, mid + 1, r);
         merge(array, l, mid, r);
     }
 
-    private <E extends Comparable<E>> void merge(E[] array, int l, int mid, int r) {
-        E[] temp = Arrays.copyOfRange(array, l, r);
+    private static <E extends Comparable<E>> void merge(E[] array, int l, int mid, int r) {
+        E[] temp = Arrays.copyOfRange(array, l, r + 1);
 
-        int i = l, j = mid;
+        int i = l, j = mid + 1;
 
-        for (int k = l; k < mid; k++) {
+        // 从做导游遍历整个数组
+        for (int k = l; k <= r; k++) {
             if (i > mid) {
-                array[k] = temp[i - k];
-                i++;
+                // 左边的数组已经完成排序
+                array[k] = temp[j - l];
+                j++;
             } else if (j > r) {
-                array[k] = temp[j - k];
+                // 右边的数组已完成排序
+                array[k] = temp[i - l];
+                i++;
+            } else if (temp[i - l].compareTo(temp[j - l]) <= 0) {
+                // 左边小于或等于右边的数值
+                array[k] = temp[i - l];
+                i++;
+            } else {
+                // 右边大于左边的数值
+                array[k] = temp[j - l];
                 j++;
             }
         }
