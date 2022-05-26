7ffafc294234e20529ddde67829428bc9a17564c
jianghao <125354243@qq.com>
2022-05-26 10:36:09 +0800

    删除掉无用的引用

--- mybatis/src/main/java/com/mybatis/intercept/ExecutorPlugin.java
+++ mybatis/src/main/java/com/mybatis/intercept/ExecutorPlugin.java
@@ -1,29 +1,14 @@
 package com.mybatis.intercept;
 
 import org.apache.ibatis.executor.Executor;
-import org.apache.ibatis.executor.statement.BaseStatementHandler;
-import org.apache.ibatis.executor.statement.StatementHandler;
-import org.apache.ibatis.javassist.Modifier;
-import org.apache.ibatis.javassist.compiler.MemberResolver;
-import org.apache.ibatis.mapping.BoundSql;
 import org.apache.ibatis.mapping.MappedStatement;
 import org.apache.ibatis.plugin.Interceptor;
 import org.apache.ibatis.plugin.Intercepts;
 import org.apache.ibatis.plugin.Invocation;
 import org.apache.ibatis.plugin.Signature;
-import org.apache.ibatis.reflection.DefaultReflectorFactory;
-import org.apache.ibatis.reflection.MetaObject;
-import org.apache.ibatis.reflection.factory.DefaultObjectFactory;
-import org.apache.ibatis.reflection.wrapper.DefaultObjectWrapperFactory;
-import org.apache.ibatis.scripting.xmltags.DynamicContext;
-import org.apache.ibatis.scripting.xmltags.SqlNode;
 import org.apache.ibatis.session.ResultHandler;
 import org.apache.ibatis.session.RowBounds;
-import org.springframework.util.StringUtils;
-import sun.reflect.misc.ReflectUtil;
 
-import java.lang.reflect.Field;
-import java.util.Map;
 import java.util.Properties;
 
 /**
