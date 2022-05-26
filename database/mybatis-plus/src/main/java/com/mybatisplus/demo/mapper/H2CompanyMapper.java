c743a596cb9971f8c897eddad969163197bde9a0
jianghao <125354243@qq.com>
2022-05-26 11:57:17 +0800

    重新分配demo案例分类

---
+++ database/mybatis-plus/src/main/java/com/mybatisplus/demo/mapper/H2CompanyMapper.java
@@ -0,0 +1,16 @@
+package com.mybatisplus.demo.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.mybatisplus.demo.model.Company;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+public interface H2CompanyMapper extends BaseMapper<Company> {
+
+    @Select("SELECT c.id, c.name, u.id AS uid, u.name AS uname, u.age, u.email " +
+            " FROM company c JOIN user u ON c.id = u.company_id WHERE c.id=1")
+    List<Company> testResultMapCollection(IPage<Company> page);
+}
