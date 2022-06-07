839020702f4ea64537c8188d85954eb5f5362f7a
jianghao <125354243@qq.com>
2022-06-07 14:03:36 +0800

    增加堆算法

---
+++ src/main/java/com/chiang/algorithm/senior/heap/MaxHeap.java
@@ -0,0 +1,51 @@
+package com.chiang.algorithm.senior.heap;
+
+
+import com.chiang.algorithm.foundation.arrary.DynamicArray;
+
+public class MaxHeap<E extends Comparable<E>> {
+    private DynamicArray<E> data;
+
+    public MaxHeap() {
+        data = new DynamicArray<>();
+    }
+
+    public MaxHeap(int capacity) {
+        data = new DynamicArray<>(capacity);
+    }
+
+    public boolean isEmpty() {
+        return data.isEmpty();
+    }
+
+    public int getSize() {
+        return data.getSize();
+    }
+
+    /**
+     * 获取父节点
+     * @param index 当前节点索引
+     */
+    public int getParent(int index) {
+        if (index == 0) {
+            throw new IllegalArgumentException("0 not have parent");
+        }
+        return (index - 1) / 2;
+    }
+
+    /**
+     * 获取节点左边的索引
+     * @param index 父节点索引
+     */
+    public int leftChild(int index) {
+        return (index * 2) + 1;
+    }
+
+    /**
+     * 获取右节点
+     * @param index 父节点索引
+     */
+    public int rightChild(int index) {
+        return (index * 2) + 2;
+    }
+}
