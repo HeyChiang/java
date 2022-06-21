44502e3e0543afeaee055e30fe22529cb0db47d5
jianghao <125354243@qq.com>
2022-06-21 14:43:18 +0800

    解决寻找第N大的堆排序问题

--- src/main/java/com/chiang/algorithm/senior/heap/FindRangeKSolution.java
+++ src/main/java/com/chiang/algorithm/senior/heap/FindRangeKSolution.java
@@ -6,9 +6,9 @@ import com.chiang.algorithm.SortHelper;
 /**
  * 输入整数数组arr ，找出其中最小的k个数。例如，输入4、5、1、6、2、7、3、8这8个数字，则最小的4个数字是1、2、3、4。
  */
-class Solution {
+class FindRangeKSolution {
 
-    public int[] getLeastNumber(Integer[] arr,int k){
+    public int[] getLeastNumber(Integer[] arr, int k) {
         PriorityQueue<Integer> queue = new PriorityQueue<>();
 
         // 使用最大堆，先保存k个数
@@ -18,14 +18,14 @@ class Solution {
 
         // 如果数字大于最大堆的第一个数字，就加入队列
         for (int i = k; i < arr.length; i++) {
-            if(!queue.isEmpty() && arr[i] < queue.getFront()){
+            if (!queue.isEmpty() && arr[i] < queue.getFront()) {
                 queue.dequeue();
                 queue.enqueue(arr[i]);
             }
         }
 
         // 将所有的结果都添加到数组中
-        int[] res =new int[k];
+        int[] res = new int[k];
         for (int i = 0; i < k; i++) {
             res[i] = queue.dequeue();
         }
@@ -35,9 +35,9 @@ class Solution {
 
     public static void main(String[] args) {
         Integer[] integers = SortHelper.generateArray(5);
-        int[] leastNumber = new Solution().getLeastNumber(integers, 3);
+        int[] leastNumber = new FindRangeKSolution().getLeastNumber(integers, 3);
         for (int j : leastNumber) {
-            System.out.print(j +" ");
+            System.out.print(j + " ");
         }
     }
 }
