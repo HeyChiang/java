085fd1b350fd10e095e7a50b4b1d88f189df5a27
jianghao <125354243@qq.com>
2022-06-22 14:45:12 +0800

    增加代码注释

--- src/main/java/com/chiang/algorithm/recursion/quick/Quick3Sort.java
+++ src/main/java/com/chiang/algorithm/recursion/quick/Quick3Sort.java
@@ -14,14 +14,17 @@ public class Quick3Sort {
     }
 
     private static void quickSortArr() {
-        Integer[] arr = SortHelper.generateArray(11);
-
+        Integer[] arr = new Integer[]{3,5,7,6,2};
+        SortHelper.print("-----原始", arr);
         sort(arr, 0, arr.length);
-        SortHelper.print("结果", arr);
+        SortHelper.print("-----结果", arr);
     }
 
     /**
-     * 对数组进行排序，
+     * 对数组进行排序，不断的将数组切分为小块的左右两个，直到无法切分。
+     *   1. 最小的数组始终在左边被排序
+     *   2. 最大的数组始终在右边被排序
+     *   3. 相同的数组放在中间不管
      */
     public static <E extends Comparable<E>> void sort(E[] arr, int l, int r) {
         if (l >= r) return;
@@ -47,7 +50,12 @@ public class Quick3Sort {
         // 最后的l放到中间去
         swap(arr, l, lt);
 
+        SortHelper.print("排序", arr,l,r);
+
+        // 对小的数组进行排序
         sort(arr, l, lt );
+
+        // 对大的数组进行排序
         sort(arr, gt, r);
     }
 
