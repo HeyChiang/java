13cba26b8853ad20a7dc3e8f692507799b627180
jianghao <125354243@qq.com>
2022-08-01 09:32:48 +0800

    增加库存失败的校验

--- ddd/src/main/java/com/ddd/product/application/eventhandler/OrderSuccessProductEventHandler.java
+++ ddd/src/main/java/com/ddd/product/application/eventhandler/OrderSuccessProductEventHandler.java
@@ -4,7 +4,6 @@ import com.ddd.infracore.event.DomainEventHandler;
 import com.ddd.order.application.dto.BuyProductDto;
 import com.ddd.order.domain.entity.Order;
 import com.ddd.order.domain.event.OrderSuccessEvent;
-import com.ddd.product.infrastructure.dataobject.ProductDO;
 import com.ddd.product.infrastructure.mapper.ProductMapper;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +11,7 @@ import org.springframework.stereotype.Component;
 
 /**
  * 订单成功事件处理
+ *
  * @author JiangHao
  */
 @Component
@@ -24,8 +24,11 @@ public class OrderSuccessProductEventHandler implements DomainEventHandler<Order
     public void onApplicationEvent(OrderSuccessEvent event) {
         Order order = (Order) event.getSource();
         for (BuyProductDto buyProductDto : order.getProductList()) {
-            productMapper.updateStock(buyProductDto.getId(),buyProductDto.getBuyNum());
+            Integer integer = productMapper.updateStock(buyProductDto.getId(), buyProductDto.getBuyNum());
+            if (integer <= 0) {
+                throw new RuntimeException(buyProductDto.getId() + "扣除库存失败");
+            }
         }
-        System.out.println("我是商品扣库存"+order.getOrderId());
+        System.out.println("我是商品扣库存" + order.getOrderId());
     }
 }
